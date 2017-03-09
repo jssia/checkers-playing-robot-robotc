e4e05789083096f61cb65d2b2c8e6a78e3ff691a
Richard Cai <RichardJCai@users.noreply.github.com>
2017-03-08 22:32:53 -0500

    Add files via upload

---
+++ robot-c.c
@@ -0,0 +1,26 @@
+int main(){
+  //Main loop
+  //Tracking distance, 24 inches by 24 inches
+  //Using 8 by 8 nested array, 3 inches represents 1 square
+
+}
+
+//Moves the x-direction motor, A is X direction motor, B is Y direction motor, C is Z direction motor
+float moveDirection(float distance,char motor){
+  motor[char] = 75;
+  while(distTravelled < distance){
+    //Code to move the motor, stops when distance travelled reaches distance
+  }
+  distTravelled = //Calculations for distance;
+
+  //PID code?
+}
+
+void resetToOrigin(){
+  //Move X,Y,Z all to 0
+  //Possibly use color/touch sensor to set to origin
+}
+
+void inputMove(){
+  //Use buttons to input a move
+}
