2d4fb9295e343b6454bf343ff53455114074e642
Richard Cai <RichardJCai@users.noreply.github.com>
2017-03-08 22:36:13 -0500

    Add files via upload

--- robot-c.c
+++ robot-c.c
@@ -2,7 +2,9 @@ int main(){
   //Main loop
   //Tracking distance, 24 inches by 24 inches
   //Using 8 by 8 nested array, 3 inches represents 1 square
-
+  //For example, moving to square D6 from the origin (0,0), D6 is 4,6
+  //The robot must move right 4 squres, so x motor moves 12 inches,
+  //Then the robot must move up 6 squres, so y motor moves 18 inches
 }
 
 //Moves the x-direction motor, A is X direction motor, B is Y direction motor, C is Z direction motor
