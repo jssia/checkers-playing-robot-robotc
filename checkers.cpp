445af1f79e03caaaaf5da4f6e7dcc731960f7453
Jin S <jssia@users.noreply.github.com>
2017-03-15 10:44:53 -0400

    Update checkers.cpp

--- checkers.cpp
+++ checkers.cpp
@@ -2,10 +2,22 @@
 #include <cmath>
 using namespace std;
 
-void boardOutput(int arr[8][8]){
+//Global 2D Array for board
+int board[8][8];
+
+void displayBoard(){
   for (int i = 7; i >= 0;i--){
       for (int j = 0; j < 8; j++){
-        cout << "[" << arr[i][j] << "]";
+        //cout << "[" << board[i][j] << "]";
+        if (board[i][j] == 0) {
+        	cout << "[ ]";
+        }
+        else if (board[i][j] == 4) {
+        	cout << "[X]";
+        }
+        else {
+        	cout << "[O]";
+        }
       }
       cout << " " << i+1 << endl;
     }
@@ -14,88 +26,45 @@ void boardOutput(int arr[8][8]){
     << "d" << "  " << "e" << "  " << "f" << "  " << "g"  << "  "<< "h";
 }
 
-void newGame(int board[8][8]){
+void newGame(){
   //I is row, J is col
+
   for (int i = 0; i < 8;i++){
       for (int j = 0; j < 8; j++){
         //Setting default pieces
 
-        //Black pieces 1
-
-        //Row 1
-        if ((i+1) == 1){
-          if ((j+1)%2 == 1){
-            board[i][j] = 1;
+        // //Black pieces 1
+        if (i < 3){
+          if (i%2 == 0){
+            if (j%2 == 0){
+              board[i][j] = 3;
+            }
           }
-          else{
-            board[i][j] = 0;
-          }
-        }
-
-        //Row 2
-        else if ((i+1) == 2){
-          if ((j+1)%2 == 0){
-            board[i][j] = 1;
-          }
-          else{
-            board[i][j] = 0;
+          else if (i%2 == 1){
+            if (j%2 == 1){
+              board[i][j] = 3;
+            }
           }
         }
-
-        //Row 3
-         else if ((i+1) == 3){
-          if ((j+1)%2 == 1){
-            board[i][j] = 1;
+        else if (i > 4){
+          if (i%2 == 0){
+            if (j%2 == 0){
+              board[i][j] = 4;
+            }
           }
-          else{
-            board[i][j] = 0;
+          else if (i%2 == 1){
+            if (j%2 == 1){
+              board[i][j] = 4;
+            }
           }
-        }
-
-        //Row 6
-        else if ((i+1) == 6){
-          if ((j+1)%2 == 1){
-            board[i][j] = 2;
-          }
-          else{
-            board[i][j] = 0;
-          }
-        }
-
-        //Row 7
-        else if ((i+1) == 7){
-          if ((j+1)%2 == 0){
-            board[i][j] = 2;
-          }
-          else{
-            board[i][j] = 0;
-          }
-        }
-
-        //Row 8
-        else if ((i+1) == 8){
-          if ((j+1)%2 == 1){
-            board[i][j] = 2;
-          }
-          else{
-            board[i][j] = 0;
-          }
-        }
-
-        //Set blank spaces to 0
-        else{
-        board[i][j] = 0;
         }
       }
     }
+    
 }
 
-bool whosTurn(){
-  return false; //keep true for now
-  //later make true if it's black turn and false if red turn
-}
-
-bool checkWinner(int board[8][8]){
+bool checkWinner(){
+  //Check if all pieces of a type are gone
   return false;
 }
 
@@ -104,70 +73,155 @@ void makeKing(){
 
 }
 
-//Checks if a piece is a king
-void checkKing(){
+bool canCapture(bool blacksTurn){
+  //Must capture pieces if available
 
-}
-
-//Checks if a move is legal, returns a boolean, called by the makeMove
-bool legalMove(int board[8][8], string curr, string to, bool king){
-  //Legal move conditions, 1 diagonal, jump over piece, jump over multiple pieces - will be hard to implement
-
-  //Check if a piece moves single piece diagonally
-  int curr1 = curr.at(0) - 97; //Converts letter input to an integer number, COLUMN INPUT
-  int curr2 = curr.at(1) - '0' - 1; //Converts char number to integer number, ROW INPUT
-
-  int to1 = to.at(0) - 97;
-  int to2 = to.at(1) - '0' - 1;
-
-  cout << endl << curr1 << curr2 << to1 << to2 << endl;
-  //For nonking black pieces - single diagonal move, can only move forward, left/right, piece must be there and destination should be empty
-  if (whosTurn()){
-    if (( (abs(to1 - curr1)) == 1) && (to2 - curr2 == 1) && (board[curr2][curr1] == 1) && (board[to2][to1] == 0) ){
-      cout << true;
-      return true;
+for (int i = 0; i < 8; i++){ //iterating through rows
+  for (int j = 0; j < 8; j++){ //iterating through columns
+  
+    if (blacksTurn){
+      //Black Piece Cases
+      if ( (board[i][j] == 1 || board[i][j] == 3) && j < 6 && ((board[i+1][j+1] == 2) && (board[i+2][j+2] == 0))){
+        return true;
+      }
+      else if ( (board[i][j] == 1 || board[i][j] == 3) && j > 1 && ((board[i+1][j-1] == 2) && (board[i-2][j-2] == 0))){
+        return true;
+      }
+      
+      //Extra King Checks
+      else if (board[i][j] == 3 && j < 6 && ( ((board[i-1][j+1] == 2) || (board[i-1][j+1] == 4)) && (board[i-2][j+2] == 0))){
+        return true;
+      }
+      else if  (board[i][j] == 3 && j > 1 && ( ((board[i-1][j-1] == 2 || board[i-1][j-1] == 4 )) && (board[i-2][j-2] == 0))){
+        return true;
+      }
+    }
+    else{
+      //Red Piece Cases
+      if ((board[i][j] == 2 || board[i][j] == 4) && j < 6 && ((board[i-1][j+1] == 1) && (board[i-2][j+2] == 0))){
+        return true;
+      }
+      else if  ((board[i][j] == 2 || board[i][j] == 4) && j > 1 && ((board[i-1][j-1] == 1) && (board[i-2][j-2] == 0))){
+        return true;
+      }
+      
+  
+      else if (board[i][j] == 4 && j < 6 && ( ((board[i+1][j+1] == 1 || board[i+1][j+1] == 3)) && (board[i+2][j+2] == 0))){
+        return true;
+      }
+      else if  (board[i][j] == 4 && j > 1 && (((board[i+1][j-1] == 1 || board[i+1][j-1] == 3)) && (board[i+2][j-2] == 0))){
+        return true;
+      }
     }
   }
-
-  //For nonking red pieces - single diagonal move, can only move forward, left/right, piece must be there and destination should be empty
-  if (!whosTurn()){
-    if (( (abs(to1 - curr1)) == 1) && (to2 - curr2 == -1) && (board[curr2][curr1] == 2) && (board[to2][to1] == 0) ){
-      cout << true;
-      return true;
-    }
   }
-
-  //Check jumping over pieces for nonking black
-  if (whosTurn()){
-    //Check if current space contains black nonking and final square is blank
-    //Then check if intermediate spaces hold correct
-  }
-
-  cout << false;
   return false;
 }
 
-void makeMove(int board[8][8]){
+void updateBoard(bool blacksTurn, int currentRow,int currentCol, int toRow, int toCol,int jumpRow, int jumpCol){
+  board[toRow][toCol] = board[currentRow][currentCol];
+  board[currentRow][currentCol] = 0;
+  
+  if (jumpRow != -1){
+    board[jumpRow][jumpCol] = 0;
+  }
+}
+
+//Checks if a move is legal, returns a boolean, called by the makeMove
+bool legalMove(bool blacksTurn, int currentRow, int currentCol, int toRow, int toCol){
+  //Legal move conditions, 1 diagonal, jump over piece, jump over multiple pieces - will be hard to implement
+  
+  int jumpRow = -1, jumpCol = -1;
+  bool legal = false;
+  //Check if a piece moves single piece diagonally
+
+  cout << endl << currentRow << currentCol << toRow << toCol << endl;
+
+  //Black moves
+  if (blacksTurn){
+    if (( (toRow - currentRow) == 1) && (abs(toCol - currentCol) == 1) && (board[currentRow][currentCol] == 1 || board[currentRow][currentCol] == 3) && (board[toRow][toCol] == 0) ){
+      legal = true;
+    }
+    else if ( (toRow - currentRow == -1) && (abs(toCol - currentCol) == 1) && (board[currentRow][currentCol] == 3) && (board[toRow][toCol] == 0) ){
+      legal = true;
+    }
+    //Jumps
+    if ( (toRow - currentRow == 2) && (toCol - currentCol == 2) && (board[currentRow][currentCol] == 1 || board[currentRow][currentCol] == 3) && board[toRow][toCol] == 0
+      && board[currentRow + 1][currentCol + 1] == 2 ){
+      jumpRow = currentRow + 1;
+      jumpCol = currentCol +1;
+      legal = true;
+    }
+    else if( (toRow - currentRow == 2) && (toCol - currentCol == -2) && (board[currentRow][currentCol] == 1 || board[currentRow][currentCol] == 3) && board[toRow][toCol] == 0
+    && board[currentRow + 1][currentCol - 1] == 2){
+      jumpRow = currentRow + 1;
+      jumpCol = currentCol - 1;
+      legal = true;
+    }
+  }
+
+  //Red moves
+  if (!blacksTurn){
+    if ( (toRow - currentRow == -1) && (abs(toCol - currentCol) == 1) && (board[currentRow][currentCol] == 2 || board[currentRow][currentCol] == 4) && (board[toRow][toCol] == 0) ){
+      legal = true;
+    }
+    else if (( (toRow - currentRow) == 1) && (abs(toCol - currentCol) == 1) && (board[currentRow][currentCol] == 4) && (board[toRow][toCol] == 0) ){
+      legal = true;
+    }
+    
+    if ( (toRow - currentRow == -2) && (toCol - currentCol == 2) && (board[currentRow][currentCol] == 2) && board[toRow][toCol] == 0
+      && board[currentRow - 1][currentCol + 1] == 1 ){
+      jumpRow = currentRow - 1;
+      jumpCol = currentCol +1;
+      legal = true;
+    }
+    else if( (toRow - currentRow == -2) && (toCol - currentCol == -2) && board[currentRow][currentCol] == 2 && board[toRow][toCol] == 0
+    && board[currentRow - 1][currentCol - 1] == 1){
+      jumpRow = currentRow + 1;
+      jumpCol = currentCol - 1;
+      legal = true;
+    }
+  }
+
+  if (legal){
+    updateBoard(blacksTurn, currentRow,currentCol,toRow,toCol,jumpRow,jumpCol);
+  }
+  return legal;
+}
+
+void makeMove(bool blacksTurn){
+  int currentRow = 0, currentCol = 0, toRow = 0, toCol = 0;
+  string curr = "x", to = "x";
+
+  cout << endl << "Force? " << canCapture(blacksTurn); // DEBUGGINGPURPOSE
   do{
-    string curr = "X",to = "X";
     cout << endl << "Which piece would you like to move? ";
     cin >> curr;
     cout << "To where? ";
-    cin >> to;} while (!legalMove(board,curr,to,false));
+    cin >> to;
+    currentCol = curr.at(0) - 97; //Converts letter input to an integer number, COLUMN INPUT
+    currentRow = curr.at(1) - '0' - 1; //Converts char number to integer number, ROW INPUT
 
-  //whosTurn = !whosTurn
+    toCol = to.at(0) - 97;
+    toRow = to.at(1) - '0' - 1;
+    cout << currentRow << currentCol <<toRow << toCol;
 
+  } while (!legalMove(blacksTurn,currentRow,currentCol,toRow,toCol));
 }
 
 int main() {
     int counter = 0;
-    int board[8][8];
+    int blacksTurn = true;
+    
+    newGame();
+    displayBoard();
 
-    newGame(board);
-    boardOutput(board);
+    while(!checkWinner()){
+      makeMove(blacksTurn);
+      cout << endl;
+      displayBoard();
+      blacksTurn = !blacksTurn;
 
-    while(!checkWinner(board)){
-      makeMove(board);
     }
 
     return 0;
